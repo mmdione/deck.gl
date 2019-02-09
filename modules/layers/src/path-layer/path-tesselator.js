e0e3c75b74328e99389f3a27766bb5c400c499e5
Xiaoji Chen <xiaoji@uber.com>
2019-02-09 15:13:16 -0800

    support non-iterable data prop

--- modules/layers/src/path-layer/path-tesselator.js
+++ modules/layers/src/path-layer/path-tesselator.js
@@ -75,7 +75,7 @@ export default class PathTesselator extends Tesselator {
         return this._updateAttribute({
           target,
           size: 3,
-          getValue: (object, index) => accessor(index)
+          getValue: (object, context) => accessor(context.index)
         });
 
       default:
