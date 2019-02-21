dafd21f344598741ef0d61164892de2576d754e6
Xiaokai Li <xiaokai@uber.com>
2019-02-21 11:16:03 -0800

    allow width in TripsLayer

--- examples/website/trips/app.js
+++ examples/website/trips/app.js
@@ -76,7 +76,7 @@ export class App extends Component {
         getPath: d => d.segments,
         getColor: d => (d.vendor === 0 ? [253, 128, 93] : [23, 184, 190]),
         opacity: 0.3,
-        strokeWidth: 2,
+        getWidth: 20,
         trailLength,
         currentTime: this.state.time
       }),
